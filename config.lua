024f35e65c0a76a477af9e4a54aae002262ac3a3
erikmeyer08 <43043270+erikmeyer08@users.noreply.github.com>
2022-03-18 15:39:22 -0400

    Updated Config Names

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 
-Config.JobsVehicleLocations = {
+Config.BossMenus = {
     ['police'] = vector4(459.0193, -979.9127, 25.3074, 360.0),
     ['ambulance'] = vector4(321.7758, -565.7872, 28.5771, 250.0),
     -- ['realestate'] = vector3(-124.786, -641.486, 168.820),
@@ -9,7 +9,7 @@ Config.JobsVehicleLocations = {
     -- ['mechanic'] = vector3(-347.41, -133.48, 39.01)
 }
 
-Config.Gangs = {
+Config.GangMenus = {
     ['lostmc'] = vector3(0, 0, 0),
     ['ballas'] = vector3(0,0, 0),
     ['vagos'] = vector3(0, 0, 0),
